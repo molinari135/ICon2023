7ce6c2fa011be103030da750550b9d207e7821cb
Ester <estermolinari135@gmail.com>
2023-01-05 11:54:14 +0100

    Update beer.pl

--- Prolog/beer.pl
+++ Prolog/beer.pl
@@ -1,6 +1,7 @@
 use_module(library(csv)).
 
-import:-
+% FATTI (DA CSV)
+:-
     csv_read_file('csv/beer_name.csv',Beer,[functor(beer),separator(0';)]),
     maplist(assert,Beer),
     csv_read_file('csv/beer_brewery.csv',Brewery,[functor(brewery),separator(0';)]),
@@ -18,7 +19,15 @@ import:-
     csv_read_file('csv/beer_review.csv',Review,[functor(review),separator(0';)]),
     maplist(assert,Review).
 
-%RULES
+%ANNOTAZIONI
+%Tutti i valori numerici vanno da 1 a 5
+%Low = [1,2.5]
+%Neutral = (2.5,3.5)
+%High = [3.5,5]
+%Il problema sta nel selezionare le birre con valori
+%non per forza esclusivi, valutando anche OR
+
+%REGOLE
 what_brewery(Beer_name):-
     beer(A,Beer_name),
     brewery(A,Brewery_name),
@@ -66,3 +75,55 @@ what_review(Beer_name):-
     beer(A,Beer_name),
     review(A,Review_value),
     write("Review: "),write(Review_value).
+
+% CLASSIFICAZIONE
+% L'utente fornisce in input i suoi "gusti"
+% e il sistema gli consiglia lo stile di birra
+% che più si avvicina ad essi
+max(X,Y,Max):-
+    X >= Y, !,
+    Max = X; Max = Y.
+
+% Identifica lo stile dell'utente
+identify_taste(Sweet_value,Bitter_value,Sour_value,Salty_value):-
+
+    taste(A,Sweet,Bitter,Sour,Salty),
+
+    Sweet is Sweet_value,
+    Bitter is Bitter_value,
+    Sour is Sour_value,
+    Salty is Salty_value,
+
+    beer(A,_Beer_name),
+    style(A,Style_name),
+    write("Recommended style: "),write(Style_name),nl.
+
+identify_mouthfeel(Astringency_value,Body_value,Alcohol_value):-
+
+    mouthfeel(A,Astringency,Body,Alcohol),
+
+    Astringency > Astringency_value,
+    Body > Body_value,
+    Alcohol > Alcohol_value,
+
+    beer(A,_Beer_name),
+    style(A,Style_name),
+    write("Recommended style: "),write(Style_name),nl.
+
+identify_flavour(Fruity_value,Hoppy_value,Malty_value,Spices_value):-
+
+    flavour(A,Fruity,Hoppy,Malty,Spices),
+
+    Fruity > Fruity_value,
+    Hoppy > Hoppy_value,
+    Malty > Malty_value,
+    Spices > Spices_value,
+
+    beer(A,_Beer_name),
+    style(A,Style_name),
+    write("Recommended style: "),write(Style_name),nl.
+
+identify_user(A,B,C,D,E,F,G,H,J,K,L):-
+    identify_flavour(A,B,C,D),
+    identify_mouthfeel(E,F,G),
+    identify_flavour(H,J,K,L).
